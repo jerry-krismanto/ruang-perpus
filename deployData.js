7c30469f5af2ed351092600a5bba43571915945b
Jerry Yan Krismanto <jerryykrismanto@gmail.com>
2023-05-22 11:42:04 +0700

    finished v1

---
+++ deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = "Jerry Yan Krismanto"; // TODO: replace this
+
+const studentId = "FE4269955"; // TODO: replace this
+
+const NetlifyDeployUrl = "TODO: replace me"; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
